6df1fc6ead1495b38957f2418f131b947acab023
kidgalance <kidgalance@gmail.com>
2021-05-13 20:37:50 +0700

    push 2

--- ptb1.js
+++ ptb1.js
@@ -1,14 +1,15 @@
 let numA = +prompt("Nhap vao he so A: ");
 let numB = +prompt("Nhap vao he so B: ");
-let x = 0;
+
 
 if(a==0){
-    alert("PT vo nghiem");
-}
-else if (b==0){
-    alert("PT co vo so nghiem");
-}
-else{
+    if(b==0){
+        alert("PT co vo so nghiem");
+    }else{
+        alert("PT vo nghiem");
+    }
+       
+}else{
     x=-numB/numA;
     alert("x co nghiem la: ",x);
 }
