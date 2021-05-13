5c30e7c85ab98195b7d03363d9aa810deaf88907
kidgalance <kidgalance@gmail.com>
2021-05-13 21:41:19 +0700

    push 2 excersice 2,3

---
+++ diemTB/diemTB.js
@@ -0,0 +1,6 @@
+let math = +prompt("Nhập vào điểm Toán: ");
+let liter = +prompt("Nhập vào điểm Văn: ");
+let eng = +prompt("Nhập vào điểm Tiếng Anh: ");
+
+ave = (math + liter + eng) / 3;
+alert("Điểm trung bình 3 môn là: " +ave);
---
+++ namsinh/yearage.js
@@ -0,0 +1,8 @@
+var currentYear = new Date();
+var year = currentYear.getFullYear();
+alert("Năm hiện tại là: " +year);
+let age = +prompt("Nhập vào năm sinh của bạn: ");
+
+yearAge = year - age;
+
+alert("Tuổi của bạn là: " +yearAge);
