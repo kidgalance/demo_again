6a2747f1de894372493776371dcfc5f94c2a97cd
kidgalance <kidgalance@gmail.com>
2021-05-18 19:46:47 +0700

    add3

--- lab_course/lai_suat/laixuat.js
+++ lab_course/lai_suat/laixuat.js
@@ -2,8 +2,8 @@ let tienVay = +prompt("Nhap vao so tien vay: ");
 let laiSuat = +prompt("Nhap vao lai suat ngan hang: ");
 let namVay = +prompt("Nhap vao so nam vay: ");
 
-tienLai = tienVay * laiSuat;
-gocLai = tienVay + (namVay*(tienLai/100));
+tienLai = tienVay * (laiSuat/100);
+gocLai = tienVay + (namVay*tienLai);
 document.write("So tien lai la: "+tienLai);
 
-document.write("So tien sau "+namvay+"ca goc lan lai la: "+gocLai+"trieu VND");
+document.write("So tien sau "+namVay+"ca goc lan lai la: "+gocLai+"trieu VND");
