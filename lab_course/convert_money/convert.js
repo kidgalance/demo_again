bf35c5646941eebb6d1944e3efe888fa028e5315
kidgalance <kidgalance@gmail.com>
2021-05-18 21:13:12 +0700

    convert money

--- lab_course/convert_money/convert.js
+++ lab_course/convert_money/convert.js
@@ -3,10 +3,10 @@ function convertMoney(){
     let amount = document.getElementById('amount').value;
     let fromCurrent = document.getElementById('from_current').value;
     let toCurrent = document.getElementById('to_current').value;
-    
-    result = amount * fromCurrent / toCurrent;
-    console.log(result);
+    //Math.floor hàm làm tròn số
+    result = Math.floor(amount * toCurrent / fromCurrent);
+    //console.log(result);
     //thay doi noi dung phan tu HTML su dung innerHTML
- //   document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = "Result: " +result;
 
 }
